3717773acf6817700aa961725a5bd1358395afb1
Aigarsss <aigars.uplejs@gmail.com>
2018-08-25 20:17:56 +0300

    includes

---
+++ app/libraries/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Base controller
+ * Loads the models and views
+ */
+
+ class Controller{
+    // Load Model
+    public function model($model){
+        // require model file
+        require_once '../app/models/' . $model . '.php';
+
+        // Instantiate the model
+        return new $model;
+
+    }
+
+    // Load view
+    public function view($view, $data=[]){
+        // Check for view file
+        if(file_exists('../app/views/' . $view . '.php')){
+            require_once '../app/views/' . $view . '.php';
+        } else {
+            //View does not eist
+            die('View does not exist');
+        }
+    }
+
+ }
